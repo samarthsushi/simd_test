467b0cebd7554f28e38cb3d81c2b4c0b4ddd34d0
samarth <samarthkulkarni@icloud.com>
2025-01-17 00:06:31 +0530

    removed fat

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,7 @@
-#![feature(test)]
 #![feature(portable_simd)]
-
-extern crate test;
-
 use std::simd::f32x8;
 use std::simd::num::SimdFloat;
-use rand::Rng;
-use rayon::prelude::*;
-
 const NUM_LANES: usize = 8;
-
-pub fn generate_random_vector(n: usize) -> Vec<f32> {
-    let mut rng = rand::thread_rng();
-    (0..n).map(|_| rng.gen_range(0.0..1.0)).collect()
-}
 
 pub fn simd_dot_product(a: &[f32], b: &[f32]) -> f32 {
     assert_eq!(a.len(), b.len());
@@ -29,8 +17,6 @@ pub fn simd_dot_product(a: &[f32], b: &[f32]) -> f32 {
     }
 
     let mut sum = sum_simd.reduce_sum();
-
-    // Handle the remaining elements that didn't fit into a SIMD chunk
     while i < a.len() {
         sum += a[i] * b[i];
         i += 1;
@@ -42,7 +28,6 @@ pub fn simd_dot_product(a: &[f32], b: &[f32]) -> f32 {
 pub fn simd_dot_product_mixed_precision(a: &[f32], b: &[f32]) -> f64 {
     assert_eq!(a.len(), b.len());
 
-    // Accumulate results in an f64 vector
     let mut sum_simd = [0.0; NUM_LANES];
 
     let mut i = 0;
@@ -67,34 +52,6 @@ pub fn simd_dot_product_mixed_precision(a: &[f32], b: &[f32]) -> f64 {
     sum
 }
 
-pub fn simd_threaded_dot_product(a: &[f32], b: &[f32]) -> f32 {
-    assert_eq!(a.len(), b.len());
-
-    // Handle the full SIMD chunks
-    let simd_sum: f32 = a.par_chunks_exact(NUM_LANES)
-        .zip(b.par_chunks_exact(NUM_LANES))
-        .map(|(a_chunk, b_chunk)| {
-            let a_simd = f32x8::from_slice(a_chunk);
-            let b_simd = f32x8::from_slice(b_chunk);
-            let product = a_simd * b_simd;
-
-            // Sum the SIMD results
-            product.reduce_sum()
-        })
-        .sum();
-
-    // Handle the remaining elements that don't fit into a full SIMD chunk
-    let remainder_sum: f32 = a.par_chunks_exact(NUM_LANES)
-        .remainder()
-        .iter()
-        .zip(b.par_chunks_exact(NUM_LANES).remainder().iter())
-        .map(|(&x, &y)| x * y)
-        .sum();
-
-    // Combine the results
-    simd_sum + remainder_sum
-}
-
 pub fn dot_product(a: &[f32], b: &[f32]) -> f32 {
     assert_eq!(a.len(), b.len());
 
@@ -103,43 +60,3 @@ pub fn dot_product(a: &[f32], b: &[f32]) -> f32 {
         .map(|(x, y)| x * y)  
         .sum()               
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use test::Bencher;
-    use std::hint::black_box;
-
-    #[bench]
-    fn bench_simd(b: &mut Bencher) {
-        let n = 64_000_000;
-        let vec_a = vec![0.000001; n];
-        let vec_b = vec![0.000001; n];
-        b.iter(|| simd_dot_product(black_box(&vec_a), black_box(&vec_b)));
-    }
-
-    #[bench]
-    fn bench_sequential(b: &mut Bencher) {
-        let n = 64_000_000;
-        let vec_a = vec![0.000001; n];
-        let vec_b = vec![0.000001; n];
-        b.iter(|| dot_product(black_box(&vec_a), black_box(&vec_b)));
-    }
-
-    #[bench]
-    fn bench_simd_mixed_precision(b: &mut Bencher) {
-        let n = 64_000_000;
-        let vec_a = vec![0.000001; n];
-        let vec_b = vec![0.000001; n];
-        b.iter(|| simd_dot_product_mixed_precision(black_box(&vec_a), black_box(&vec_b)));
-    }
-
-    #[bench]
-    fn bench_simd_threaded(b: &mut Bencher) {
-        let n = 64_000_000;
-        let vec_a = vec![0.000001; n];
-        let vec_b = vec![0.000001; n];
-        b.iter(|| simd_threaded_dot_product(black_box(&vec_a), black_box(&vec_b)));
-    }
-}
-
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use simd_test::{simd_dot_product_mixed_precision, simd_dot_product, simd_threaded_dot_product, dot_product};
+use simd_test::{simd_dot_product_mixed_precision, simd_dot_product, dot_product};
 use std::time::Instant;
 
 fn main() {
-    let n = 64_000_000;
+    let n = 1_000_000;
     let vec_a = vec![0.000001; n];
     let vec_b = vec![0.000001; n];
 
@@ -18,12 +18,8 @@ fn main() {
     let simd_mixed_result = simd_dot_product_mixed_precision(&vec_a, &vec_b);
     let simd_mixed_dur = start.elapsed();
 
-    let start = Instant::now();
-    let simd_threaded_result = simd_threaded_dot_product(&vec_a, &vec_b);
-    let simd_threaded_dur = start.elapsed();
-
-    println!("SIMD:\nDot product: {}\nDuration: {:?}\n\n", simd_result, simd_dur);
-    println!("Sequential:\nDot product: {}\nDuration: {:?}\n\n", seq_result, seq_dur);
-    println!("SIMD Mixed Precision:\nDot product: {}\nDuration: {:?}\n\n", simd_mixed_result, simd_mixed_dur);
-    println!("SIMD Threaded Precision:\nDot product: {}\nDuration: {:?}\n\n", simd_threaded_result, simd_threaded_dur);
+    println!(
+        "SIMD:\nDot product: {}\nDuration: {:?}\n\nSequential:\nDot product: {}\nDuration: {:?}\n\nSIMD Mixed Precision:\nDot product: {}\nDuration: {:?}"
+        , simd_result, simd_dur, seq_result, seq_dur, simd_mixed_result, simd_mixed_dur
+    );
 }
